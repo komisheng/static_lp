5f17a6beaba3c0f81e2c5970e1451397b75b8e17
komisheng <hongyesheng@dtbcreation.com>
2025-10-17 16:11:04 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Custom JavaScript for landing page
+;(($) => {
+  // Declare the jQuery variable before using it
+  var jQuery = window.jQuery
+
+  // Add any custom interactions here
+  $(document).ready(() => {
+    console.log("Landing page loaded")
+  })
+})(window.jQuery)
